abefc30e976e4ab36d9363aef3eaa939c1dd5da3
Bartek Sadlej <barteksadlej@gmail.com>
2023-05-02 09:49:25 +0200

    config

---
+++ config/airflow_local_settings.py
@@ -0,0 +1,11 @@
+# $AIRFLOW_HOME/config/airflow_local_settings.py
+
+from airflow.models import DAG
+from airflow.exceptions import AirflowClusterPolicyViolation
+
+def dag_policy(dag: DAG):
+    """Ensure that DAG has at least one tag"""
+    if not dag.tags:
+        raise AirflowClusterPolicyViolation(
+            f"DAG {dag.dag_id} has no tags. At least one tag required. File path: {dag.fileloc}"
+        )
